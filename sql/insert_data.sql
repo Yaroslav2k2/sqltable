2baa19eedf7205b14fcb75f56ace2b5665f6a44e
Yaroslav2k2 <140788545+Yaroslav2k2@users.noreply.github.com>
2024-08-25 21:27:47 +0400

    Create insert_data.sql

---
+++ sql/insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, firstname, lastname, email) VALUES 
+(1, 'Reuben', 'Nienow', 'arlo50@example.org'),
+(2, 'Frederik', 'Upton', 'terrence.cartwright@example.org'),
+(3, 'Unique', 'Windler', 'rupert55@example.org'),
+-- другие записи, если необходимо
+;
